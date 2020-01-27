0ae879b7973773e5f23d4e37d8db70eabf8318f1
Ash Peterson <ashnight07@gmail.com>
2020-01-27 09:41:21 -0600

    Print out n fibonacci numbers

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,14 +1,14 @@
 def fibonacci_recursive(number)
   return 0 if number.zero?
   return 1 if number == 1
-  fibonacci_recursive(number - 1) + fibonacci_recursive(number - 2) if number > 1
+  return fibonacci_recursive(number - 1) + fibonacci_recursive(number - 2) if number > 1
 end
 
 def fibonacci_iterative(number)
   num1, num2 = 0, 1
   p num1, num2
 
-  (number - 1).times do |index|
+  (number - 1).times do
     next_num = num1 + num2
 
     p next_num
@@ -17,6 +17,9 @@ def fibonacci_iterative(number)
   end
 end
 
-puts fibonacci_recursive(9)
+num_fibs = 9
+num_fibs.times do |fibonacci_num|
+  puts fibonacci_recursive(fibonacci_num)
+end
 
-fibonacci_iterative(9)
+#fibonacci_iterative(9)
